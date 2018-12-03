06f9246c10f97f8c091834eb072d81a7e3b63375
PRITHABANERJEE1994 <pritha.banerjee@dineout.co.in>
2018-12-03 15:32:44 +0530

    updated reservation

--- inresto/src/app/app.module.ts
+++ inresto/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
 import { LoggerService } from './logger.service';
 import { SharedModule } from './shared/shared.module';
@@ -15,7 +15,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedModule,
     CommonviewsModule,
